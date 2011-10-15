29378da62d9bde3b848c8ef3709edb66c633cea2
XANI <xani666@gmail.com>
2011-10-15 16:59:18 +0200

    move etckeeper to puppet module

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -6,9 +6,6 @@ $modules = "$repo/modules"
 $puppet_header = "DPP/Puppet managed file at location $location, DO NOT EDIT BY HAND, changes will be overwritten."
 
 node default {
-    package {'etckeeper':
-        ensure => 'latest',
-    }
     include puppet
     file { "/tmp/i_am_puppet":
         content => "DPP: puppet ver $puppetversion on $hostname; facter ver $facterversion",
--- puppet/modules/puppet/manifests/init.pp
+++ puppet/modules/puppet/manifests/init.pp
@@ -1,6 +1,11 @@
 class puppet {
     #source => "$base::modules/puppet/files/emacslibs",
     $puppet_repo_path = '/var/lib/dppd/repo'
+
+    package {'etckeeper':
+        ensure => 'latest',
+    }
+
     file { puppet-conf:
         path => '/etc/puppet/puppet.conf',
         content => template('puppet/puppet.conf.erb'),
